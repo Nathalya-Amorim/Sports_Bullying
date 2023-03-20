ed360296248efc8486d35cd89fc08fdb23a30d79
Nathalya Amorim <nathalyamorim87@gmail.com>
2023-03-19 23:40:49 -0400

    Fixing footer and minor bugs.

--- src/components/VideoSection.vue
+++ src/components/VideoSection.vue
@@ -3,8 +3,7 @@
         <div class="  embed-responsive embed-responsive-21by9">
             <!-- <iframe allow="fullscreen" controls autoplay="false" class="embed-responsive-item"
                 src="video/promo-video.webm"></iframe> -->
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/R_yx2N0E21c" title="YouTube video player"
-                frameborder="0"
+            <iframe src="https://www.youtube.com/embed/R_yx2N0E21c" title="YouTube video player" frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowfullscreen></iframe>
         </div>
